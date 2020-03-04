dcd0aa369ef6b884c933db86f7c809fd17b06ffa
Joe Clark <clark_joe@bah.com>
2020-03-04 01:03:46 -0500

    first commit

---
+++ db/V1__setup.sql
@@ -0,0 +1,25 @@
+CREATE SCHEMA IF NOT EXISTS `default`;
+
+DROP TABLE IF EXISTS default.sitemap;
+CREATE TABLE default.sitemap (
+    sitemap_id VARCHAR(256) PRIMARY KEY,
+    url VARCHAR(256),
+    alias VARCHAR(256),
+    title VARCHAR(256),
+    skip_update boolean,
+    skip_migrate boolean,
+    duplicate VARCHAR(256)
+);
+
+DROP TABLE IF EXISTS default.agency;
+CREATE TABLE default.agency (
+    agency_id VARCHAR(256) PRIMARY KEY,
+    name VARCHAR(256)
+);
+
+INSERT INTO default.agency (agency_id, name)
+VALUES
+  ('ofccp', 'Office of Federal Contract Compliance Programs'),
+  ('arb', 'Administrative Review Board'),
+  ('brb', 'Benefits Review Board'),
+  ('ecab', 'Employees Compensation Appeals Board');
